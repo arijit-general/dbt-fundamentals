d8bb1d2a461f458176da6192e53f0733cb7f4d3d
arijit.mondal@gmail.com <arijit.mondal@gmail.com>
2022-01-10 19:49:24 +0000

    all_dates.sql added

---
+++ models/marts/core/int_orders__pivoted.sql
@@ -0,0 +1,22 @@
+ {%- set payment_methods =['bank_transfer','coupon','credit_card','gift_card'] -%}
+
+with payment as (
+  select *
+  from {{source('stripe','payment')}}
+),
+pivoted as (
+    select
+        orderid,
+       
+
+        {%- for payment_method in payment_methods %}
+        sum(case when paymentmethod = '{{payment_method}}' then {{cents_to_dollars('amount')}} else 0 end ) as {{payment_method}}_amount  {%- if not loop.last -%} , {%- endif %}
+
+        {%- endfor -%}
+  
+     from payment
+     where status = 'success'
+     group by 1
+)
+
+select * from pivoted
